ae4ec1845a4a6c932fcb6efe8418b22118ddb894
Hedgehog-maker <sanasvid22@gmail.com>
2026-05-08 17:49:53 +0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Меняем заголовок
-document.getElementById("title").innerHTML = "Добро пожаловать!";
-
-// Добавляем обработчик кнопки
-document.getElementById("myButton").onclick = function() {
-    alert("Кнопка нажата!");
-};
